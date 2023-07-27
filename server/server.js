2eafca071d86aae6e4862aa42b894ef1aa0d045c
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-07-27 19:51:04 -0400

    adds middleware

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const db = require('./db/index');
 const app = express();
 const port = process.env.PORT || 3005;
 
+// middleware
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Server listening on Port: ${port}`);
 }); 
