a3724ca092ba3d967bcc2bd12fa14f8b193fcf3b
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-07-27 20:11:13 -0400

    Created route to retrieve all patients in db

--- server/server.js
+++ server/server.js
@@ -7,6 +7,28 @@ const port = process.env.PORT || 3005;
 // middleware
 app.use(express.json());
 
+// get all patients
+app.get('/patients', async(req, res) => {
+  try {
+    const patients = await db.query(
+      'SELECT * FROM patient_information'
+    );
+    console.log(patients.rows)
+    res.status(200).json(
+      {
+        status: 'Success!',
+        results: patients.rows.length,
+        data: {
+          patients: patients.rows
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err)
+  };
+});
+
+// Listens on port for connections
 app.listen(port, () => {
   console.log(`Server listening on Port: ${port}`);
-}); 
+});
