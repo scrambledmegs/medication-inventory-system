76eb269237653bcf314e54b8606527af1d559213
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-07-30 18:36:39 -0400

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,25 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 function App() {
   return (
     <>
+    <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+        <Navbar.Brand href="#home">Pyxis</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to='/patientlist'>Patient List</Nav.Link>
+              <NavDropdown title="More Options" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/patientlist' element={<PatientList />} />
