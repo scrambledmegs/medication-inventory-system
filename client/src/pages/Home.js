0cd27a48200fb1afb33803ed245c91d17be35cf8
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-07-31 18:12:48 -0400

    created link to patient list via button. adds rows and cols.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
+import { Link } from 'react-router-dom';
+
+// Bootstrap
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Medication Admin/Inventory System</h1>
-    </div>
-  )
+    <Container>
+      <Row className='test '>
+        <Col>
+          <h1>Medication Admin/Inventory System</h1>
+          <div className='click-begin'>
+            <Link to='/patientlist'>
+              <Button variant='primary'>Click to Begin</Button>
+            </Link>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Home
