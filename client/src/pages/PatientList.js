c9896c678a44a145e972ac2daeca189fd0620de6
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-07-31 18:21:15 -0400

    fetches api data to render list of patients. bootstrap list styling

--- client/src/pages/PatientList.js
+++ client/src/pages/PatientList.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import PatientData from '../apis/PatientData';
+import { PatientsContext } from '../context/PatientsContext';
 
-const PatientList = () => {
+// Bootstrap 
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const PatientList = (props) => {
+  const { patients, setPatients } = useContext(PatientsContext)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await PatientData.get('/');
+        setPatients(response.data.data.patients);
+      } catch (err) {
+        console.log(err)
+      };
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>PatientList</div>
+    <div>
+      <h1>Patient List</h1>
+      {patients.map(patient => {
+        return (
+          <ListGroup as='ul'>
+            <ListGroup.Item as='li' variant='primary'>
+              {patient.patient_name}
+            </ListGroup.Item>
+            <ListGroup.Item as='li'>
+              {patient.patient_mrn}
+            </ListGroup.Item>
+            <ListGroup.Item as='li'>
+              {patient.patient_dob}
+            </ListGroup.Item>
+            <ListGroup.Item as='li'>
+              {patient.patient_allergies}
+            </ListGroup.Item>
+            <ListGroup.Item as='li'>
+              {patient.room_number}
+            </ListGroup.Item>
+            <ListGroup.Item as='li'>
+              {patient.department}
+            </ListGroup.Item>
+          </ListGroup>
+        )
+      })}
+    </div>
   )
 }
 
-export default PatientList
+export default PatientList;
