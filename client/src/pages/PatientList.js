e36ad4d4b127d8e4d4f73d1471599810af23ae30
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-08-01 20:46:57 -0400

    created delete patient functionality

--- client/src/pages/PatientList.js
+++ client/src/pages/PatientList.js
@@ -4,6 +4,7 @@ import { PatientsContext } from '../context/PatientsContext';
 
 // Bootstrap 
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button'
 
 const PatientList = (props) => {
   const { patients, setPatients } = useContext(PatientsContext)
@@ -12,7 +13,8 @@ const PatientList = (props) => {
     const fetchData = async () => {
       try {
         const response = await PatientData.get('/');
-        setPatients(response.data.data.patients);
+        console.log('RESPONSE PL:', response)
+        setPatients(response.data.data.patients)
       } catch (err) {
         console.log(err)
       };
@@ -20,13 +22,28 @@ const PatientList = (props) => {
     fetchData();
   }, []);
 
+    const handleDelete = async(id) => {
+      try {
+        const response = await PatientData.delete(`/${id}`)
+        setPatients(patients.filter(patient => {
+          return patient.id !== id
+        }))
+      } catch (err) {
+        console.log(err)
+      }
+    };
+
   return (
     <div>
       <h1>Patient List</h1>
       {patients && patients.map(patient => {
         return (
-          <ListGroup as='ul' key={patient.patient_id}>
-            <ListGroup.Item as='li' variant='primary'>
+          <ListGroup as='ul' key={patient.id}>
+            <ListGroup.Item 
+              as='li' 
+              variant='primary'
+              // onClick={() => handleSelectPatient(patient.id)}
+              >
               {patient.patient_name}
             </ListGroup.Item>
             <ListGroup.Item as='li'>
@@ -43,6 +60,12 @@ const PatientList = (props) => {
             </ListGroup.Item>
             <ListGroup.Item as='li'>
               {patient.department}
+            </ListGroup.Item>
+            <ListGroup.Item as='li'>
+              <Button
+                onClick={()=> handleDelete(patient.id)}
+              >
+                Discharge Patient</Button>
             </ListGroup.Item>
           </ListGroup>
         )
