f2a76333701834500d46366510a2cf84893de693
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-08-01 20:50:38 -0400

    adds more context

--- client/src/context/PatientsContext.js
+++ client/src/context/PatientsContext.js
@@ -4,9 +4,16 @@ export const PatientsContext = createContext()
 
 export const PatientsContextProvider = props => {
   const [patients, setPatients] = useState([])
+  const [selectedPatient, setSelectedPatient] = useState([])
 
   return (
-    <PatientsContext.Provider value={{patients: patients, setPatients}}>
+    <PatientsContext.Provider 
+      value={{
+        patients, 
+        setPatients, 
+        selectedPatient, 
+        setSelectedPatient}}
+    >
       {props.children}
     </PatientsContext.Provider>
   )
