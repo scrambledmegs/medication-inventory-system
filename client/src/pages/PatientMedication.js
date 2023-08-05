c6ce6b2d11c96b34e5138cf5737bfa287c705306
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-08-04 20:34:03 -0400

    adds function to remove medication when button clicked

--- client/src/pages/PatientMedication.js
+++ client/src/pages/PatientMedication.js
@@ -1,13 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { PatientContext } from '../context/PatientContext';
+import MedicationData from '../apis/MedicationData';
+
+// Bootstrap
+import Button from 'react-bootstrap/Button';
 
 const PatientMedication = () => {
-  const { selectedMedication } = useContext(PatientContext)
+  const { 
+    selectedMedication, 
+    setSelectedMedication 
+  } = useContext(PatientContext);
+
+  console.log('SELECTED MEDICATION IN PM:', selectedMedication)
+
+  const updateMedicationQuantity = (e) => {
+    console.log('E:', e)
+    MedicationData.put(`/${selectedMedication.medication_id}`, selectedMedication).then((resp) => {
+      console.log('my data', resp.data);
+      setSelectedMedication((prevMed) => {
+        console.log(prevMed)
+        return {
+          ...prevMed,
+          quantity: selectedMedication.quantity -1
+        };
+      });
+    });
+  };
 
   return (
     <div>
       <h1>Patient Medication</h1>
       {selectedMedication.med_name}
+      <br />
+      {selectedMedication.quantity}
+      <br />
+      <Button 
+        onClick={() => updateMedicationQuantity()}
+      >
+        Remove Medication
+      </Button>
     </div>
   );
 };
