7aa53eb1ba822a03ebcdb5ea5e25ef089166a365
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-08-10 22:18:31 -0400

    converts file to jsx, removes Button import from Bootstrap

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Home.css';
+
+// React Router
+import { Link } from 'react-router-dom';
+
+// Bootstrap
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Home = () => {
+  return (
+    <main>
+      <Container>
+        <Row className='heading'>
+          <Col>
+            <h1>Medication Admin/Inventory System</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className='click-begin'>
+              <Link to='/patients'>
+                <button className='home-btn'>Click to Begin</button>
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  );
+};
+
+export default Home;
