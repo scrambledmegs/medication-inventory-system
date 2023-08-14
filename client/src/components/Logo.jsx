50b5f45bc714f928a06419c10796da9aa01ad039
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-08-14 18:48:07 -0400

    adds logo component

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../img/logo.png';
+
+const Logo = () => {
+  return (
+    <main>
+      <img className='logo' src={logo} alt='PharmaLogix logo'/>
+    </main>
+  );
+};
+
+export default Logo;
