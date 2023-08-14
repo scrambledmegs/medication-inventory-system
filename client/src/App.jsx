718926c08658f28215c192aa15d8bba4d39fbe03
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-08-13 20:37:18 -0400

    adds semantic html tag, imports assignmed file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,13 @@ import AdmitPatient from './pages/AdmitPatient';
 import PatientMedication from './pages/PatientMedication';
 import MedicationList from './pages/MedicationList';
 import AssignMedication from './pages/AssignMedication';
-import UpdateQuantity from './pages/UpdateQuantity';
+import UpdateInventoryForm from './pages/UpdateInventoryForm';
 import MedicationForm from './pages/MedicationForm';
+import './App.css';
+
+// Context Providers
 import { PatientContextProvider } from './context/PatientContext';
 import { MedicationContextProvider } from './context/MedicationContext';
-import './App.css';
 
 // React Router
 import { Route, Routes } from 'react-router-dom';
@@ -24,37 +26,37 @@ import Row from 'react-bootstrap/Row';
 function App() {
 
   return (
-    <div className='App'>
-    <PatientContextProvider >
-      <MedicationContextProvider>
-        <Container>
-          <Row>
-            <NavigationBar />
-          </Row>
-          <Routes> 
-            <Route path='/' element={<Home />} />
-            <Route path='/admitPatient' element={<AdmitPatient />} />
-            <Route path='/patients' element={<PatientList />} />
-            <Route path='/patients/:patientid' element={<Patient />} />
-            <Route 
-              path = '/patients/:patientid/:medicationid' 
-              element={<PatientMedication />} 
-            />
-            <Route path='/medications' element={<MedicationList />} />
-            <Route path ='/medicationForm' element={<MedicationForm />} />
-            <Route 
-              path='/medications/:medicationid/update' 
-              element={<UpdateQuantity />} 
-            />
-            <Route 
-              path='/assignMedication' 
-              element={<AssignMedication />} 
-            />
-          </Routes>
-        </Container>
-      </MedicationContextProvider>
-    </PatientContextProvider>
-    </div>
+    <main className='App'>
+      <PatientContextProvider >
+        <MedicationContextProvider>
+          <Container>
+            <Row>
+              <NavigationBar />
+            </Row>
+            <Routes> 
+              <Route path='/' element={<Home />} />
+              <Route path='/admitPatient' element={<AdmitPatient />} />
+              <Route path='/patients' element={<PatientList />} />
+              <Route path='/patients/:patientid' element={<Patient />} />
+              <Route 
+                path = '/patients/:patientid/:medicationid' 
+                element={<PatientMedication />} 
+              />
+              <Route path='/medications' element={<MedicationList />} />
+              <Route path ='/medicationForm' element={<MedicationForm />} />
+              <Route 
+                path='/medications/:medicationid/update' 
+                element={<UpdateInventoryForm />} 
+              />
+              <Route 
+                path='/assignMedication' 
+                element={<AssignMedication />} 
+              />
+            </Routes>
+          </Container>
+        </MedicationContextProvider>
+      </PatientContextProvider>
+    </main>
   );
 };
 
